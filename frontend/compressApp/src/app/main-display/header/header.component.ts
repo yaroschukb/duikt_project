c47492498ab7e8ca82b2073294612df717ea4d57
yaroschukb <gitmongonode2020@gmail.com>
2024-12-11 14:28:55 +0200

    dev: add log in and sign up form

--- frontend/compressApp/src/app/main-display/header/header.component.ts
+++ frontend/compressApp/src/app/main-display/header/header.component.ts
@@ -1,11 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzModalComponent } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NzButtonModule],
+  imports: [
+    NzButtonModule,
+    NzFormModule,
+    NzModalComponent,
+    ReactiveFormsModule,
+    CommonModule,
+    NzCheckboxModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isSignUpVisible = false;
+  isLogInVisible = false;
+  signUpForm: FormGroup;
+  logInForm: FormGroup;
+  loading = false;
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    this.logInForm = this.fb.group({
+      username: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+      remember: this.fb.control(true),
+    });
+  }
+  showModal(i: string | number): void {
+    i === 1 ? (this.isSignUpVisible = true) : (this.isLogInVisible = true);
+  }
+
+  handleCancel(): void {
+    this.isSignUpVisible = false;
+    this.isLogInVisible = false;
+    this.signUpForm.reset();
+    this.logInForm.reset();
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      this.loading = true;
+      console.log('Form data:', this.signUpForm.value);
+    }
+  }
+}
