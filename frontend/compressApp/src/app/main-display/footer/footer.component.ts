08ff446f058e089303f5764954152ec185f8d6e7
yaroschukb <gitmongonode2020@gmail.com>
2024-12-11 02:01:05 +0200

    dev: add styles buttons

--- frontend/compressApp/src/app/main-display/footer/footer.component.ts
+++ frontend/compressApp/src/app/main-display/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NzIconModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
--- frontend/compressApp/src/app/main-display/header/header.component.ts
+++ frontend/compressApp/src/app/main-display/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NzButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
