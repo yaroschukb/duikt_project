505a225f9c344bc5482c4e9ce8978ede41aa868c
yaroschukb <gitmongonode2020@gmail.com>
2024-12-14 08:29:13 +0200

    ref: update styles

--- frontend/compressApp/src/app/main-display/footer/footer.component.ts
+++ frontend/compressApp/src/app/main-display/footer/footer.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  FacebookOutline,
+  TwitterOutline,
+  InstagramOutline,
+} from '@ant-design/icons-angular/icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,5 +12,10 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
   imports: [NzIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
+  providers: [
+    { provide: 'icon-facebook-o', useValue: FacebookOutline },
+    { provide: 'icon-twitter-o', useValue: TwitterOutline },
+    { provide: 'icon-instagram-o', useValue: InstagramOutline },
+  ],
 })
 export class FooterComponent {}
